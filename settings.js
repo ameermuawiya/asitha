5cc30291ce227a21a48804700a9489b10c8544ba
ameermuawiya <81509990+ameermuawiya@users.noreply.github.com>
2025-06-24 16:11:52 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=4UQAgJqJ#U322-En6wxS6M618RmbcI8YrKpSb0XiF8GIJlwKbU_k' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝐋ᴀᴋᴀ-𝐌ᴅ=dwZHwYJQ#VOz9YywC5Qnp_kjArxDr8BZ9xp0GX_RznZvT7FtmM8k' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:@Asitha2005b@db.waiqbrnuxkjebghzhovz.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL,
